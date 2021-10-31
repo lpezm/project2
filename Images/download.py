7785d0f9978b62dec06099af852f5e38bdb8a712
lpezm <91690696+lpezm@users.noreply.github.com>
2021-10-31 17:40:07 +0000

    Add files via upload

--- Images/download.py
+++ Images/download.py
@@ -9,7 +9,9 @@ print(captchaNames)
 for i in captchaNames.split(',\n'):
     file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=lpezm&download=resume_speed&myfilename=' + i
     files = requests.get(file_url)
+    print("Time for file retrieval is:","--- %s seconds ---" % (time.time() - start_time))
     file = open(i,"wb")
     file.write(files.content)
     file.close()
+    print("Time for file retrieval is:","--- %s seconds ---" % (time.time() - start_time))
 print("Time for file retrieval is:","--- %s seconds ---" % (time.time() - start_time))
